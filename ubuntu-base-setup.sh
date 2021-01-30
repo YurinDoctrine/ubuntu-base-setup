def2b9a67060df019257cea35ea598e96f7186af
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-01-30 20:32:54 +0300

    Add files via upload

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -98,6 +98,7 @@ PKGS=(
     # GENERAL UTILITIES ---------------------------------------------------
     \
     'flameshot'    # Screenshots
+    'file-roller'  # Create and modify archives
     'freerdp'      # RDP Connections
     'libvncserver' # VNC Connections
     'filezilla'    # FTP Client
