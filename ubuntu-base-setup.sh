80413bdb1f1d45d092e565cb7506745ad7f2991d
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-05-29 12:57:10 +0300

    Add files via upload

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -157,7 +157,7 @@ sudo sed -i -e 's|#HandleLidSwitch=suspend|HandleLidSwitch=suspend|g' /etc/syste
 
 # ------------------------------------------------------------------------
 
-echo "Disabling buggy cursor inheritance"
+echo -e "Disabling buggy cursor inheritance"
 # When you boot with multiple monitors the cursor can look huge. This fixes that...
 echo -e "[Icon Theme]
 #Inherits=Theme
