75c676ae6fecc15cbf876888dde98c945e1bc04e
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-08-22 23:40:58 +0300

    Edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -143,14 +143,14 @@ sudo sed -i -e '/floppy/d' /etc/fstab
 
 extra() {
     cd /tmp
-    curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/ultra-gaming-setup-wizard.sh >ultra-gaming-setup-wizard.sh &&
+    curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/YurinDoctrine/ultra-gaming-setup-wizard/main/ultra-gaming-setup-wizard.sh >ultra-gaming-setup-wizard.sh &&
         chmod 0755 ultra-gaming-setup-wizard.sh &&
         ./ultra-gaming-setup-wizard.sh
 }
 
 extra2() {
     cd /tmp
-    curl -fsSL https://raw.githubusercontent.com/YurinDoctrine/secure-linux/master/secure.sh >secure.sh &&
+    curl --tlsv1.2 -fsSL https://raw.githubusercontent.com/YurinDoctrine/secure-linux/master/secure.sh >secure.sh &&
         chmod 0755 secure.sh &&
         ./secure.sh
 }
