85a57d20992c72bea6252a9f25edc06fffa8e731
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-08-29 09:47:43 +0300

    Edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -22,12 +22,12 @@ echo -e 'Acquire::Languages "none";' | sudo tee /etc/dpkg/dpkg.cfg.d/90nolanguag
 # ------------------------------------------------------------------------
 
 # Setting up locales & timezones
-echo -e "LANG=en_GB.UTF8" | sudo tee -a /etc/locale.conf
 echo -e "LANG=en_GB.UTF8" | sudo tee -a /etc/environment
 echo -e "LC_ALL=en_GB.UTF8" | sudo tee -a /etc/environment
 sudo sed -i -e 's/^#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/' /etc/locale.gen
 sudo apt install --reinstall --purge -y locales
 sudo dpkg-reconfigure -f noninteractive locales
+sudo locale-gen en_GB.UTF-8
 timedatectl set-ntp true
 timedatectl set-timezone Europe/Moscow
 
