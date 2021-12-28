6c4be0a6382aa886c6a993d70b6d6b7e9b8c68cf
YURIN <43861005+YurinDoctrine@users.noreply.github.com>
2021-12-28 13:20:57 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -318,7 +318,7 @@ sudo apt-get autoremove -yy --purge
 # ------------------------------------------------------------------------
 
 ## Optimize font cache
-mkfontscale && mkfontdir && fc-cache -fv && fc-cache ~/.fonts
+fc-cache -fv && fc-cache ~/.fonts
 
 # ------------------------------------------------------------------------
 
