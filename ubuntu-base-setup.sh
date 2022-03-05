172e84be3df4b249ac726d1d9ac7b4d42fb58d14
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-05 22:54:25 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -331,6 +331,12 @@ sudo sed -i -e 's/#HandleLidSwitch=.*/HandleLidSwitch=suspend/' /etc/systemd/log
 
 # ------------------------------------------------------------------------
 
+echo -e "Disable bluetooth autostart"
+sudo sed -i -e 's/AutoEnable.*/AutoEnable=false/' /etc/bluetooth/main.conf
+sudo sed -i -e 's/#FastConnectable.*/FastConnectable = false/' /etc/bluetooth/main.conf
+
+# ------------------------------------------------------------------------
+
 ## GRUB timeout
 sudo sed -i -e 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=1/' /etc/default/grub
 ## Change GRUB defaults
