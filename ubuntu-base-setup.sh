ac113b812c7c8236ab62612c4c169b3cec9f8c63
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-07 22:18:42 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -85,7 +85,11 @@ PKGS=(
 
     # GENERAL UTILITIES ---------------------------------------------------
 
-    'irqbalance' # IRQ balancing daemon for SMP systems
+    'irqbalance'  # IRQ balancing daemon for SMP systems
+    'pipewire'    # Low-latency audio/video router and processor
+    'wireplumber' # Modular session / policy manager for PipeWire
+
+    # DEVELOPMENT ---------------------------------------------------------
 
 )
 
