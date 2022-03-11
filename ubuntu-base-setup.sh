17fc105350fdb2aed64975d64b4fd194b58597f3
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-11 13:07:09 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Before hop in
 sudo apt update &&
-    sudo apt install -f --assume-yes 9base binutils curl fonts-terminus git gnupg2 haveged kmod pkgconf psmisc software-properties-common ufw zstd wget xdg-utils &&
+    sudo apt install -f --assume-yes 9base binutils curl fonts-terminus git gnupg2 haveged kmod libinput-dev pkgconf psmisc software-properties-common ufw zstd wget xdg-utils &&
     sudo apt install -f --assume-yes ubuntu-drivers-common &&
     sudo apt install -f --assume-yes kubuntu-restricted-addons
 
