1d7cd1ddaadea935a338d72c2f53d6a7c3d2d337
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-17 11:56:39 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -79,7 +79,7 @@ gsettings set org.gnome.desktop.interface enable-animations false
 # Display
 gsettings set org.gnome.desktop.interface scaling-factor 1
 gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
-gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'x11-randr-fractional-scaling']"
+gsettings set org.gnome.mutter experimental-features "['x11-randr-fractional-scaling'"', '"'scale-monitor-framebuffer']"
 
 # Keyboard
 gsettings set org.gnome.desktop.peripherals.keyboard delay 500
