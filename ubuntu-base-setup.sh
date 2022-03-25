9bb21c9c086b53c3ad9cbec8fd45693ff2c450aa
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-25 16:38:26 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -120,6 +120,7 @@ PKGS=(
     'chrony'      # Versatile implementation of the Network Time Protocol
     'dbus-broker' # Linux D-Bus Message Broker
     'mksh'        # MirBSD Korn Shell
+    'numad'       # Simple NUMA policy support
     'preload'     # Makes applications run faster by prefetching binaries and shared objects
     'tumbler'     # D-Bus service for applications to request thumbnails
 
