7b7214c95e86d7e9fc43d81309ad86954996383a
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-29 00:21:10 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -349,6 +349,8 @@ sudo sed -i -e 's/#HandleLidSwitch=.*/HandleLidSwitch=suspend/' /etc/systemd/log
 echo -e "Disable bluetooth autostart"
 sudo sed -i -e 's/AutoEnable.*/AutoEnable=false/' /etc/bluetooth/main.conf
 sudo sed -i -e 's/#FastConnectable.*/FastConnectable = false/' /etc/bluetooth/main.conf
+sudo sed -i -e 's/ReconnectAttempts.*/ReconnectAttempts = 1/' /etc/bluetooth/main.conf
+sudo sed -i -e 's/ReconnectIntervals.*/ReconnectIntervals = 1/' /etc/bluetooth/main.conf
 
 # ------------------------------------------------------------------------
 
