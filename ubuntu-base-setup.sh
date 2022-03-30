14d6985b3362c1e4195ff0bd42f5493201797bf7
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-03-30 21:20:19 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -95,7 +95,6 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat
 # Set environment variables
 echo -e "INTEL_BATCH=1
 WL_OUTPUT_SUBPIXEL_NONE=none
-LIBGL_ALWAYS_SOFTWARE=1
 LIBGL_THROTTLE_REFRESH=1
 WGL_SWAP_INTERVAL=1
 DISABLE_RTKIT=1
