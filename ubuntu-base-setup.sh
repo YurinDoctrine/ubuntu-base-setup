bb0c2775ed76d4de3c44cec21736360ab36330ee
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-06-08 20:21:22 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -100,7 +100,8 @@ gsettings set org.gnome.mutter edge-tiling true
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "KEYTIMEOUT=1
+echo -e "EGL_PLATFORM=x11
+KEYTIMEOUT=1
 GST_VAAPI_ALL_DRIVERS=1
 DRAW_USE_LLVM=1
 SOFTPIPE_USE_LLVM=1
