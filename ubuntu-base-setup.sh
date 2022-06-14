7cc0222d4531da8f2d9825015510ec26e8f51ec1
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-06-14 09:15:18 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -100,7 +100,8 @@ gsettings set org.gnome.mutter edge-tiling true
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "DRAW_NO_FSE=1
+echo -e "LIBGL_ALWAYS_INDIRECT=1
+DRAW_NO_FSE=1
 WLR_RENDERER=vulkan
 GDK_GL=gles
 CLUTTER_BACKEND=x11
