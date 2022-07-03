504300cea2b553feab5e9b2117995f149f61c238
Yurin Doctrine <43861005+YurinDoctrine@users.noreply.github.com>
2022-07-03 11:30:37 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -100,7 +100,8 @@ gsettings set org.gnome.mutter edge-tiling true
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "VGL_READBACK=pbo
+echo -e "WLR_DRM_NO_ATOMIC=1
+VGL_READBACK=pbo
 SDL_VIDEO_X11_DGAMOUSE=0
 SDL_VIDEO_FULLSCREEN_HEAD=0
 WLR_DRM_NO_MODIFIERS=1
