1ea161c6df20969ca762e96ccad6af66d5096b79
Yurin Doctrine <doctriney@protonmail.com>
2023-04-24 08:47:48 +0300

    edit ubuntu-base-setup.sh

--- ubuntu-base-setup.sh
+++ ubuntu-base-setup.sh
@@ -140,7 +140,7 @@ kwriteconfig5 --file kwalletrc --group Wallet --key "First Use" --type bool fals
 # ------------------------------------------------------------------------
 
 # Set environment variables
-echo -e "MALLOC_OPTIONS=2f
+echo -e "MALLOC_CHECK=0
 MALLOC_TRACE=0
 LIBGL_DEBUG=0
 HISTCONTROL=eraseboth
